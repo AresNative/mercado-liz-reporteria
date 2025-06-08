46b5b88388aa3fcca5e891d2b00ea52032e2051f
Juan José Eusebio <82086484+AresNative@users.noreply.github.com>
2025-06-08 15:58:07 -0700

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: ["@typescript-eslint"], // Plugin de TypeScript
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error", // Prohibir `any` explícito
+    },
+  },
 ];
 
 export default eslintConfig;
