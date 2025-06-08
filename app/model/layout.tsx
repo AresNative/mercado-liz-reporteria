73b74cc190a970e90c39c3212006c0b4ac9c80cb
Juan José Eusebio <82086484+AresNative@users.noreply.github.com>
2025-06-08 16:01:38 -0700

    Update layout.tsx

--- app/model/layout.tsx
+++ app/model/layout.tsx
@@ -1,28 +1,41 @@
-// components/DashboardLayout.tsx (Client Component)
-"use client";
-
 import { ReactNode } from "react";
+import { cookies } from "next/headers";
+
 import { DashboardLayoutProps } from "@/utils/types/interfaces";
+
 import Providers from "@/hooks/provider";
 
-const DashboardLayout = ({
-  admin,
-  user,
-  userRole, // Recibe el rol como prop
-}: DashboardLayoutProps & { userRole: string }) => {
+const DashboardLayout = async ({ admin, user }: DashboardLayoutProps) => {
+  // Función para obtener el rol del usuario desde las cookies en el lado del servidor
+  const getCookie = async (cookieName: string) => {
+    const cookieStore = await cookies(); // Acceso sincrónico a las cookies
+    const cookie = cookieStore.get(cookieName);
+    return cookie ? cookie.value : "";
+  };
+
+  // Obtenemos el rol del usuario directamente desde las cookies (ya que estamos en el lado del servidor)
+  const userRole = await getCookie("user-role");
+
+  // Contenido a mostrar según el rol del usuario
   const roleContent: Record<string, ReactNode> = {
     admin: admin,
     user: user,
   };
 
+  // Renderizado en el servidor
   return (
-    <>
-      {userRole && roleContent[userRole] ? (
-        <Providers>{roleContent[userRole]}</Providers>
-      ) : (
-        <>Acceso no autorizado</>
-      )}
-    </>
+    <section className="pt-10" >
+      {/* Muestra el contenido según el rol o un mensaje por defecto */}
+      {
+        userRole && roleContent[userRole] ? (
+          <Providers>
+            {roleContent[userRole]}
+          </Providers>
+        ) : (
+          <>Acceso no autorizado</>
+        )
+      }
+    </section >
   );
 };
 
