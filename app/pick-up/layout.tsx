0dbf67de3824b7d5e5e0673ba3ec6d0354fc70fd
Juan José Eusebio <juanerpgr@gmail.com>
2025-06-13 11:19:07 -0700

    feat: eliminar la propiedad 'ventas' del layout y ajustar la gestión de roles de usuario

--- app/pick-up/layout.tsx
+++ app/pick-up/layout.tsx
@@ -2,10 +2,9 @@ import { ReactNode } from "react";
 import { getLocalStorageItem } from "@/utils/functions/local-storage";
 import { getCookieinPage } from "@/utils/functions/cookies";
 
-const Layout = async ({ admin, user, ventas }: {
+const Layout = async ({ admin, user }: {
   admin: React.ReactNode
   user: React.ReactNode
-  ventas: React.ReactNode
 }) => {
   // Función para obtener el rol del usuario desde las cookies o localStorage
   // Si no se encuentra en las cookies, se busca en localStorage
@@ -13,8 +12,7 @@ const Layout = async ({ admin, user, ventas }: {
 
   const roleContent: Record<string, ReactNode> = {
     admin,
-    user,
-    ventas
+    user
   };
 
   return (
