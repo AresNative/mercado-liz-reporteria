ff66135a6bcd98d08e85e5e45e7385274298bcb8
Juan José Eusebio <juanerpgr@gmail.com>
2025-06-17 14:58:16 -0700

    feat: ajustar espaciado en el pie de página del ModalPedidos

--- app/pick-up/components/modal.tsx
+++ app/pick-up/components/modal.tsx
@@ -211,7 +211,7 @@ const ModalPedidos = ({ name, title, idListas, idPedido }: ModalProps) => {
                             </li>
                         </ul>
 
-                        <footer className="flex flex-wrap justify-end gap-3 pt-4 mt-4 border-t">
+                        <footer className="flex flex-wrap justify-end gap-2 pt-4 mt-4 border-t">
                             <button
                                 onClick={() => alert("Incompleto")}
                                 className="bg-red-500 text-white px-4 py-2 rounded-md"
